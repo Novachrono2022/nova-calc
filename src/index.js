e0d6a87c475e3a87618bf26bce046d80b36fdf20
Novachrono2022 <141578455+Novachrono2022@users.noreply.github.com>
2023-12-09 19:04:05 +0000

    implemented sc functions

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
         .replace("×", "*")
         .replace("÷","/")
         .replace("%", "*0.01")
+        .replace("sin", "Math.sin")
+        .replace("cos", "Math.cos")
+        .replace("ln", "Math.log")
+        .replace("π", "Math.PI")
+        .replace("log", "Math.log10")
+        .replace("e", "Math.exp")
+        .replace("tan", "Math.tan")
+        .replace("√", "Math.sqrt")
         const result = eval(convertedValue);
         currentValue = result.toString();
         display.value = currentValue;
@@ -16,19 +24,25 @@ document.addEventListener("DOMContentLoaded", function () {
         const button = buttons[i]
         button.addEventListener("click", function () {
             const value = button.innerText;
-
-            if (value == "AC") {
-                currentValue = "";
+            try {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (value == "=") {
+                    evaluateResult();
+                }
+    
+                else {
+                    currentValue += value;
+                    display.value += button.innerText;
+                }
+    
+            } catch(error){
+                console.error(error);
+                currentValue = "ERROR";
                 display.value = currentValue;
-            } else if (value == "=") {
-                evaluateResult();
             }
-
-            else {
-                currentValue += value;
-                display.value += button.innerText;
-            }
-
+            
         })
 
     }
